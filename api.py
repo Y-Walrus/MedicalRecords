92629ac81ab97e3742deba0f120391d18c3792f0
Yegor <58791613+Y-Walrus@users.noreply.github.com>
2021-10-26 21:39:33 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -5,20 +5,28 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.exceptions import InvalidSignature
+from hashlib import sha256
+from time import time, gmtime
 
-
-# watch videos in ethereum documentation
 
 # with open('private_key.pem', 'wb') as f:
 #    f.write(pem)
 
-# choice of asymmetric algorithm - why RSA?
+# choice of asymmetric algorithm - RSA?
+# move to cmd
+# broadcasting a mined block
+# public addresses by public key? how do i know who is the recipient
+# do i send the whole chain every time? or only when person joins?
+# store chain in files. json? every transaction in a different json?
+# do some private info?
 
 class Account:  # get/set functions, _private_key
     def __init__(self, nonce=0, code_hash=""):
         self.nonce = nonce  # a counter that indicates the number of transactions sent from the account. This ensures transactions are only processed once. In a contract account, this number represents the number of contracts created by the account.
         self.code_hash = code_hash  # this hash refers to the code of an account on the Ethereum virtual machine (EVM). Contract accounts have code fragments programmed in that can perform different operations. This EVM code gets executed if the account gets a message call. It cannot be changed unlike the other account fields. All such code fragments are contained in the state database under their corresponding hashes for later retrieval. This hash value is known as a codeHash. For externally owned accounts, the codeHash field is the hash of an empty string.
         self.private_key = generate_private_key()
+
+    # def __str__(self):
 
     def generate_public_address(self):
         pass
@@ -29,33 +37,66 @@ class Account:  # get/set functions, _private_key
 
 class Transaction:  # public key here?
     def __init__(self, sender, recipient, data):
+        self.sender = sender
         self.recipient = recipient  # the receiving address
         self.signature = sign(data,
                               sender.private_key)  # the identifier of the sender. This is generated when the sender's private key signs the transaction and confirms the sender has authorised this transaction
         self.data = data
 
+    def __str__(self):
+        s = ""
+        s += "Transaction ({0} to {1}): {2}".format(t.sender, t.recipient, t.data)
+        return s
+
 
 class Block:
     # generator for block_number?
-    def __init__(self, timestamp, block_number, difficulty, mix_hash, parent_hash, transactions, state_root, nonce):
-        self.timestamp = timestamp  # the time when the block was mined.
+    # see timestamp format (0/"")
+    # nonce can be a hash
+    def __init__(self, block_number, parent_hash):
+        self.timestamp = 0  # the time when the block was mined.
         self.block_number = block_number  # the length of the blockchain in blocks.
-        self.difficulty = difficulty  # the effort required to mine the block.
-        self.mix_hash = mix_hash  # a unique identifier for that block.
+        self.difficulty = DIFFICULTY  # the effort required to mine the block.
         self.parent_hash = parent_hash  # the unique identifier for the block that came before (this is how blocks are linked in a chain).
-        self.transactions = transactions  # the transactions included in the block.
-        self.state_root = state_root  # the entire state of the system: account balances, contract storage, contract code and account nonces are inside.
-        self.nonce = nonce  # a hash that, when combined with the mixHash, proves that the block has gone through proof of work.
+        self.transactions = []  # the transactions included in the block.
+        # self.state_root = state_root  # the entire state of the system: account balances, contract storage, contract code and account nonces are inside.
 
-    def add_transaction(self):
-        pass
+        self.mix_hash = ""  # a unique identifier for that block.
+        self.nonce = 0  # a hash that, when combined with the mixHash, proves that the block has gone through proof of work.
+
+    def __str__(self):
+        s = ""
+        s += "Block No. {0}\n".format(self.block_number)
+        ts = gmtime(self.timestamp)
+        s += "Mined: {0}/{1}/{2} {3:02}:{4:02}:{5:02} UTC\n".format(
+            ts.tm_mday, ts.tm_mon, ts.tm_year, ts.tm_hour, ts.tm_min, ts.tm_sec)
+        s += "MixHash: {0}\n".format(self.mix_hash)
+        for t in self.transactions:
+            s += "Transaction ({0} to {1}): {2}\n".format(t.sender, t.recipient, t.data)
+        s += "Diff: {0}\n".format(self.difficulty)
+        s += "Nonce: {}\n".format(self.nonce)
+        s += "Parent hash: {0}".format(self.parent_hash)
+        return s
+
+    def set_mix_hash(self):
+        s = str(self.block_number) + str(self.timestamp) + str(self.difficulty) + str(self.parent_hash)
+        for t in self.transactions:
+            # is str good enough here?
+            s += str(t.sender) + str(t.recipient) + str(t.data)
+        self.mix_hash = sha256(s.encode()).hexdigest()
+
+    def add_transaction(self, transaction):
+        # public key attribute for Account?
+        if verify_signature(transaction.data, transaction.signature,
+                            generate_public_key(transaction.sender.private_key)):
+            self.transactions.append(transaction)
+        else:
+            print("NOT VALID TRANSACTION: " + str(transaction))
 
 
-# class Pool
+# class Pool on mining side / just list?
 
-
-def mine():
-    pass
+DIFFICULTY = 5
 
 
 def generate_private_key():
@@ -142,20 +183,106 @@ def cryptography_check():
     print(d_data)
 
 
+def hashing_check():
+    h = sha256("a".encode())
+    print(h)
+    print(h.digest())
+    print(h.hexdigest())
+    print(h.hexdigest().startswith("00"))
+    i = 0
+    while not sha256(str(i).encode()).hexdigest().startswith("00000000"):
+        i += 1
+    print(i)
+    print(sha256(str(i).encode()).hexdigest())
+
+
+def mine_block(block):
+    block.set_mix_hash()
+    nonce = 0
+    #while not sha256((block.mix_hash + str(nonce)).encode()).hexdigest().startswith("0" * block.difficulty):
+    while not sha256((block.mix_hash + str(nonce)).encode()).hexdigest().startswith("0" * block.difficulty):
+        nonce += 1
+    block.nonce = nonce
+    block.timestamp = time()
+    return True  # break in the middle if takes too long?
+
+
 def execute_transaction(transaction):  # or execute block?
     pass
 
 
 # cryptography_check()
+# hashing_check()
 
-a1 = Account()
-a2 = Account()
+# print_private_key(a1.private_key)
+# print_private_key(a2.private_key)
 
-print_private_key(a1.private_key)
-print_private_key(a2.private_key)
 
-# what happens with the block after creation? mining somewhere
-b1 = Block()
+if __name__ == "__main__":
+    genesis_block = Block(0, "GENESIS")
+    mine_block(genesis_block)
 
-t1 = a1.create_transaction(a2, "Hello world!".encode())
-b1.add_transaction(t1)
+    chain_state = [genesis_block]
+    pending_transactions = []
+
+    a1 = Account()
+    a2 = Account()
+
+    b1 = ""
+    while True:
+        cmd = input("> ")
+        if cmd == "create block":
+            b1 = Block(len(chain_state), chain_state[-1].mix_hash)
+            print("Block created successfully")
+            # print(b1)
+
+        elif cmd == "mine block":
+            if isinstance(b1, Block) and mine_block(b1):
+                chain_state.append(b1)
+                b1 = ""
+                print("Mined successfully and added to the current state")
+
+        elif cmd == "create transaction":
+            data = input("data -> ")
+            pending_transactions.append(a1.create_transaction(a2, data.encode()))
+            print("Transaction created successfully and appended to pending transactions")
+
+        elif cmd == "add to block":
+            if isinstance(b1, Block):
+                if len(pending_transactions):
+                    while len(pending_transactions):
+                        b1.add_transaction(pending_transactions[0])
+                        pending_transactions.pop(0)
+                    print("Added transaction(s) to block successfully")
+                else:
+                    print("No pending transactions")
+            else:
+                print("No block created")
+
+        elif cmd == "print chain":
+            for b in chain_state:
+                print(b)
+                if b != chain_state[-1]:
+                    print()
+
+        elif cmd == "print pending":
+            if len(pending_transactions):
+                for t in pending_transactions:
+                    print(t)
+            else:
+                print("No pending transactions")
+
+        elif cmd == "verify last block":
+            block = chain_state[-1]
+            mix_hash = block.mix_hash
+            nonce = str(block.nonce)
+            print("Verification of block No. " + str(block.block_number))
+            print("MixHash: " + mix_hash)
+            print("Nonce: " + nonce)
+            print("Hash of " + mix_hash + nonce + " is:")
+            print(sha256((mix_hash + nonce).encode()).hexdigest())
+
+        else:
+            print("No such command")
+
+        print()
